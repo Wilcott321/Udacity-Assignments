09e956f8fd6fc746c7219ff846ac7e8697339cbe
SheThrives11 <ewilson110@gmail.com>
2016-06-27 22:05:49 -0500

    added name and role to #header <div> using jQuery
    Steps are in the comments

--- Resume/js/resumeBuilder.js
+++ Resume/js/resumeBuilder.js
@@ -1,1 +1,14 @@
-$("#header").append("E. Wilson").css({"color": "#f5a623"});
+/* Step 1: Define a variable for your name and then use the replace method 
+ to replace data in HTMLheaderName with the value stored in the name variable */
+var name = "E. Wilson";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+/* Step 2: Do the same for your role. Replace the data in the HTMLheaderRole
+variable with the value stored */
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+/* Step 3: Use the prepend method to add both your name and role to the 
+<div> with the id of "header" */
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
