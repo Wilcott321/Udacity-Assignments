3dc46aed5df0862c351ee98a0902baff1d9245d5
SheThrives11 <ewilson110@gmail.com>
2016-07-02 12:01:38 -0500

    added bio object to file for name, contact info, role, skills, and a pic

--- Resume/js/resumeBuilder.js
+++ Resume/js/resumeBuilder.js
@@ -12,3 +12,19 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 <div> with the id of "header" */
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+/* Step 4: Crete an object for your bio. Inside the bio save your name,
+role, contact info, picture url, a welcome message, and skills as properties*/
+var bio = {
+	"name" : "E. Wilson",
+	"role" : "Web Developer",
+	"contactInfo" : {
+		mobile : "000.000.0000";
+		email : "example@email.com";
+		github : "@Wilcott321";
+		location : "Illinois, USA"
+	},
+	"welcomeMessage" : "Hey there! I'm E. Wilson, a web aficionado specializing in frontend UI development and JavaScript programming.",
+	"skills" : ["HTML5", "CSS", "Bootstrap", "JavaScript", "jQuery"],
+	"bioPic" : "images/bioPic.png"
+};
