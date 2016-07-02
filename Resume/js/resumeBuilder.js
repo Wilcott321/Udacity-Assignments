83e495e2aaff44af08a357d51a47a7174dbd74d3
SheThrives11 <ewilson110@gmail.com>
2016-07-02 12:29:04 -0500

    updated bio object, appended some properties to the HTML #header

--- Resume/js/resumeBuilder.js
+++ Resume/js/resumeBuilder.js
@@ -1,30 +1,21 @@
-/* Step 1: Define a variable for your name and then use the replace method 
- to replace data in HTMLheaderName with the value stored in the name variable */
-var name = "E. Wilson";
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-/* Step 2: Do the same for your role. Replace the data in the HTMLheaderRole
-variable with the value stored */
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-/* Step 3: Use the prepend method to add both your name and role to the 
-<div> with the id of "header" */
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-/* Step 4: Crete an object for your bio. Inside the bio save your name,
-role, contact info, picture url, a welcome message, and skills as properties*/
 var bio = {
 	"name" : "E. Wilson",
 	"role" : "Web Developer",
-	"contactInfo" : {
-		mobile : "000.000.0000";
-		email : "example@email.com";
-		github : "@Wilcott321";
-		location : "Illinois, USA"
+	"contacts" : {
+		"mobile" : "000.000.0000",
+		"email" : "example@email.com",
+		"github" : "@Wilcott321",
+		"twitter" : "@OurCodeBlog",
+		"location" : "Illinois, USA"
 	},
-	"welcomeMessage" : "Hey there! I'm E. Wilson, a web aficionado specializing in frontend UI development and JavaScript programming.",
+	"welcomeMsg" : "Hey there! I'm E. Wilson, a web aficionado specializing in frontend UI development and JavaScript programming.",
 	"skills" : ["HTML5", "CSS", "Bootstrap", "JavaScript", "jQuery"],
 	"bioPic" : "images/bioPic.png"
 };
+/* The header */
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
